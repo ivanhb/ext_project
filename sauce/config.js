934587d823bc9a8ddba0f1099293380fbd243a11
ivan.heibi <ivanhb.ita@gmail.com>
2019-07-16 18:08:54 +0200

    first commit

---
+++ sauce/config.js
@@ -0,0 +1,33 @@
+var config_file = {
+
+  "sauce": {
+    "fixed_items": [
+      "f_item_0": {"options":[],"filter":{"quantity":[1,2,3,4,5]}},
+      "f_item_1": {"options":[],"filter":{"quantity":[1,2,3,4,5]}},
+      "f_item_2": {"options":[],"filter":{"quantity":[1,2,3,4,5]}}
+    ],
+    "optional_items": [
+      "opt_item_0": {"filter":{"quantity":[1,2,3,4,5]}},
+      "opt_item_1": {"filter":{"quantity":[1,2,3]}},
+      "opt_item_2": {"filter":{"quantity":[1,2,3,4,5]}},
+      "opt_item_3": {"filter":{"quantity":[1,2,3,4,5,6,7]}},
+    ]
+  },
+
+  "tag": {
+    //1st one is the default
+    "optional_items": [
+      "opt_item_0": {"img": "","filter":{"color":["red","blue","black"]}},
+      "opt_item_1": {"img": "","filter":{"color":["red","blue","black"]}},
+    ]
+  },
+
+  "bottle": {
+    //1st one is the default
+    "optional_items": [
+      "opt_item_0": {"img":""},
+      "opt_item_1": {"img": ""}
+    ]
+  }
+
+}
